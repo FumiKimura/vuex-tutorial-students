4adac14e32eddfc961eefb568037eb62730ea5ea
Norifumi Kimura <norical496@gmail.com>
2021-08-28 17:32:48 +0900

    Implemented Vuex assginments

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -2,18 +2,30 @@
   <div>
     <h3>Add Todo</h3>
     <div class="add">
+      <form v-on:submit="submitPost">
+        <input type="text" v-model="inputText" placeholder="Add new ToDo...">
+        <input type="submit">
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
 
 export default {
   name: "AddTodo",
   data: () => {
+    return {
+      inputText:''
+    }
   },
   methods: {
-
+    ...mapActions(['postTodos']),
+    submitPost(e) {
+      e.preventDefault();
+      this.postTodos(this.inputText); 
+    }
   }
 }
 </script>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -2,19 +2,34 @@ import axios from 'axios';
 
 const state = {
   //define your state properties here
+  todos:[]
 };
 
 const getters ={
  //getter methods here
+ allTodos: () => state.todos
 };
-
 
 const actions ={
  //define your actions here
+  fetchTodos: async ({ commit }) => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+    commit('setTodos', response.data);
+  },
+  postTodos: async ({ commit }, title) => {
+    const response = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false});
+    commit('addTodo', response.data);
+  }
 };
 
 const mutations ={
  //definite your mutations here
+ setTodos: (state, todos) => {
+  state.todos = todos;
+ },
+ addTodo:(state, newTodo) => {
+  state.todos.unshift(newTodo);
+ }
 };
 
 
